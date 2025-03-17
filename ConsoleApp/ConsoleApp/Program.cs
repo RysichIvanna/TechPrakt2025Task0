1f38bfe16cab99dbb5fb41bb24f943f536f65749
RysichIvanna <zipz231_rio@student.ztu.edu.ua>
2025-03-17 18:51:37 +0200

    main task

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введіть перше число: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Введіть друге число: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int sum = num1 + num2;
+            Console.WriteLine($"Сума: {sum}");
+        }
+    }
+}
